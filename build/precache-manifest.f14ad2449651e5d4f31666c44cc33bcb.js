7a6af1e2fbc40dc81d75870be34fb553b3cc24d0
skaisanlahti <skaisanlahti@gmail.com>
2020-10-14 10:47:27 +0300

    Testing

---
+++ build/precache-manifest.f14ad2449651e5d4f31666c44cc33bcb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d85a995030e85f0f6c37ab548fcaa02",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "8e23e0508ce5ef13f316",
+    "url": "/memory-game/static/css/main.c2e83791.chunk.css"
+  },
+  {
+    "revision": "4d71217393fb717de65c",
+    "url": "/memory-game/static/js/2.e604e58f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.e604e58f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e23e0508ce5ef13f316",
+    "url": "/memory-game/static/js/main.335128d2.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  },
+  {
+    "revision": "3321a66ff17219603e7464db95cfeb27",
+    "url": "/memory-game/static/media/ael-awin.3321a66f.jpg"
+  },
+  {
+    "revision": "4b645585e80f21a2a32a0a74082b562a",
+    "url": "/memory-game/static/media/ael-clone-harry-flameu.4b645585.jpg"
+  },
+  {
+    "revision": "36c6ddacc0294032679f55634ece1f4b",
+    "url": "/memory-game/static/media/ael-colland-duke-nio-2.36c6ddac.jpg"
+  },
+  {
+    "revision": "cd3b2aee0ea45d0eabb324aa1d0a7cd5",
+    "url": "/memory-game/static/media/ael-colland-duke-nio-3.cd3b2aee.jpg"
+  },
+  {
+    "revision": "47f65365cd6cb8abfa4c4b18cefbcd0e",
+    "url": "/memory-game/static/media/ael-colland-duke-nio.47f65365.jpg"
+  },
+  {
+    "revision": "53dde260742294b965573d6b1aab2001",
+    "url": "/memory-game/static/media/ael-complete-guide-cover.53dde260.jpg"
+  },
+  {
+    "revision": "508c85e7688e4ec18a1b83d30f77212c",
+    "url": "/memory-game/static/media/ael-escha-and-logy.508c85e7.jpg"
+  },
+  {
+    "revision": "86bf9bb4b18fc0b43384f6e2416d7d49",
+    "url": "/memory-game/static/media/ael-escha.86bf9bb4.jpg"
+  },
+  {
+    "revision": "8d6b216ce1c400861fb5ba0225b026f9",
+    "url": "/memory-game/static/media/ael-logix-ficsario.8d6b216c.jpg"
+  },
+  {
+    "revision": "71477bfb32ee4fe4d333c059b66a4034",
+    "url": "/memory-game/static/media/ael-logy-copy.71477bfb.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6faa159bcc4c38ebb0a2c145376be52.js"
+  "/memory-game/precache-manifest.f14ad2449651e5d4f31666c44cc33bcb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memory-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
